a57116be71c92ff606b5221b5c72bc99f6c2effa
Amstalden <lucas-amstalden@hotmail.com>
2021-05-14 00:55:50 -0300

    Continuação do curso. PC casa

---
+++ CursoWebModernoCompleto2020/node/moduloA.js
@@ -0,0 +1,5 @@
+console.log(this)
+// Formas de exportar algo de um arquivo
+this.ola = 'Fala Pessoal'
+exports.bemVindo = 'Bem vindo ao node!'
+module.exports.ateLogo = 'Até o próximo exemplo'
---
+++ CursoWebModernoCompleto2020/node/pastaA/pastaB/acessarModulo.js
@@ -0,0 +1,11 @@
+const moduloA = require('../../moduloA')
+console.log(moduloA.ola)
+
+const c = require('./pastaC')
+console.log(c.ola2)
+
+// const http = require('http')
+// http.createServer((req, res) => {
+//     res.write('Bom dia')
+//     res.end()
+// }).listen(8080)
