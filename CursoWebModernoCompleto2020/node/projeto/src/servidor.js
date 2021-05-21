eb32394f42404d9f7a05ab79391ed07a945ffadf
Amstalden <lucas-amstalden@hotmail.com>
2021-05-20 22:53:14 -0300

    Continuação curso. PC casa.

---
+++ CursoWebModernoCompleto2020/node/projeto/src/servidor.js
@@ -0,0 +1,43 @@
+const porta = 3003
+
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const bancoDeDados = require('./bancoDeDados')
+
+app.use(bodyParser.urlencoded({ extended: true })) // Pra qualquer requisição no server,
+                                                // vai passar por esse middleware
+
+app.get('/produtos', (req, res, next) => {
+    res.send(bancoDeDados.getProdutos()) // Converter para JSON
+})
+
+app.get('/produtos/:id', (req, res, next) =>{
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto) // JSON
+})
+
+app.put('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto) // JSON
+})
+
+app.delete('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.excluirProduto(req.params.id)
+    res.send(produto) // JSON
+})
+
+app.listen(porta, () => {
+    console.log(`Servidor está executando na porta ${porta}.`)
+})
