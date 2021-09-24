a2e8cb1583b0cd9dcf4495b41d36c476db7b6829
Amstalden <lucas-amstalden@hotmail.com>
2021-09-23 22:04:15 -0300

    Upload de arquivos ccom ajax

--- CursoWebModernoCompleto2020/ajax/server.js
+++ CursoWebModernoCompleto2020/ajax/server.js
@@ -7,4 +7,27 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.get('/teste', (req, res) => res.send('Ok'))
+
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: function(req, file, callback) {
+        callback(null, './upload')
+    },
+    filename: function(req, file, callback) {
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
+const upload = multer({ storage }).single('arquivo')
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if(err){
+            return res.end('Ocorreu um erro.')
+        }
+        res.send('Concluído com sucesso.')
+    })
+})
+
 app.listen(8080, () => console.log('Executando....'))
