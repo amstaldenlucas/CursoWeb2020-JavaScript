ae9fa5b3b1f630eba053fc905648cea64e60cd69
Amstalden <lucas-amstalden@hotmail.com>
2021-09-28 21:39:26 -0300

    primeiro exemplo. Juntar arquivo e remover espaços

---
+++ CursoWebModernoCompleto2020/gulp/javascript/gulpfile.js
@@ -0,0 +1,25 @@
+const { series } = require('gulp')
+const gulp = require('gulp')
+const concat = require('gulp-concat')
+const uglify  = require('gulp-uglify')
+const babel = require('gulp-babel')
+
+function transformacaoJS(cb) {
+    gulp.src('src/**/*.js')
+        .pipe(babel({
+            comments: false,
+            presets: ["env"]
+        }))
+        .pipe(uglify())
+        .on('error', err => console.log(err))
+        .pipe(concat('codigo.min.js'))
+        .pipe(gulp.dest('build'))
+    return cb()
+}
+
+function fim(cb) {
+    console.log('Fim!!')
+    return cb()
+}
+
+exports.default = series(transformacaoJS, fim)
